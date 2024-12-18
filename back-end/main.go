0a25a3aad324ab5ed87975a6537b73a059b4c760
Mohammed Aidi <mohammed.aidi@neovia.co>
2024-12-18 12:09:13 +0100

    update code 18/12/24

--- back-end/main.go
+++ back-end/main.go
@@ -12,8 +12,9 @@ func main() {
 	http.HandleFunc("/", controllers.FormAsciiArt)//GET
 	http.HandleFunc("/ascii-art", controllers.GenerateAsciiArt)
 
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("../front-end/static/css"))))
-	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("../front-end/static/js"))))
+	//configures the server to serve JavaScrip end CSS files located in the "../front-end/static/" directory from the base URL "/js/"
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("../front-end/static/css/"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("../front-end/static/js/"))))
 
 	fmt.Println("starting server on port :8081 ...")
 	if err := http.ListenAndServe(":8081", nil); err != nil {
